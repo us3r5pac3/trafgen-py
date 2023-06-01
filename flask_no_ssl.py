f6a12f5b3ac9fa017b59d429810ac671ad9c781c
us3r5pac3 <87878728+us3r5pac3@users.noreply.github.com>
2023-05-31 21:07:21 -0500

    first commit with flask and requirements files

---
+++ flask_no_ssl.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return {'ok': 'fine'}
+@app.route("/large")
+def large():
+    return {'ok': '1234' * 20000}
+@app.route('/form', methods=['POST'])
+def form():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    # return f"Received form data: Name - {name}, Email - {email}"
+    return f""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+    pass
