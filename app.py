4fbb7a6a0ad3953de8a0a41de56bb93f3b7f2594
us3r5pac3 <87878728+us3r5pac3@users.noreply.github.com>
2023-05-31 21:28:18 -0500

    changed port to 8000

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def form():
     # return f"Received form data: Name - {name}, Email - {email}"
     return f""
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8000)
     pass
